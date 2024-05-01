4014169e794a62978375b6b2012ff0193375eb51
binar-rizki <zainalrtf@gmail.com>
2024-05-01 19:40:51 +0700

    update list product

--- src/components/Product/ListProduct.js
+++ src/components/Product/ListProduct.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Button, Alert } from 'react-bootstrap';
 import axios from 'axios';
+import { Navbar, Footer } from '../Dashboard'; 
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -12,7 +13,7 @@ const ProductList = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:3000/api/products'); 
-      setProducts(response.data.data);
+      setProducts(response.data);
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
@@ -28,61 +29,69 @@ const ProductList = () => {
   };
 
   return (
-    <div className="container mt-4">
-      <h1 className="text-center mb-4">Daftar Produk</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Product ID</th>
-            <th>Nama</th>
-            <th>Harga</th>
-            <th>Deskripsi</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.length === 0 ? (
+    <>
+      <Navbar />
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Daftar Produk</h1>
+        <Table striped bordered hover>
+          <thead>
             <tr>
-              <td colSpan="5">
-                <Alert variant="danger">
-                  <b>Produk belum tersedia</b>
-                </Alert>
-              </td>
+              <th>Product ID</th>
+              <th>Nama</th>
+              <th>Harga</th>
+              <th>Deskripsi</th>
+              <th>Kategori</th> 
+              <th>Action</th>
             </tr>
-          ) : (
-            products.map((product) => (
-              <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.nama_produk}</td>
-                <td>Rp. {product.harga}</td>
-                <td>{product.deskripsi}</td>
-                <td>
-                  <Button variant="info" href={`/product/=${product.id}`}>
-                    Details
-                  </Button>
-                  <Button
-                    variant="danger"
-                    onClick={() => {
-                      if (window.confirm('Apakah Anda yakin ingin menghapus data ini?')) {
-                        handleDelete(product.id);
-                      }
-                    }}
-                  >
-                    Delete
-                  </Button>
-                  <Button variant="warning" href={`/edit-product/edit=${product.id}`}>
-                    Edit
-                  </Button>
+          </thead>
+          <tbody>
+            {products.length === 0 ? (
+              <tr>
+                <td colSpan="6"> {/* Perlu penyesuaian karena ada 6 kolom */}
+                  <Alert variant="danger">
+                    <b>Produk belum tersedia</b>
+                  </Alert>
                 </td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </Table>
-      <Button variant="primary" href="/add-product">
-        Tambahkan Produk
-      </Button>
-    </div>
+            ) : (
+              products.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.nama_produk}</td>
+                  <td>Rp. {product.harga}</td>
+                  <td>{product.deskripsi}</td>
+                  <td>{product.kategori}</td>
+                  <td>
+                    <Button variant="info" href={`/product/=${product.id}`}>
+                      Details
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        if (window.confirm('Apakah Anda yakin ingin menghapus data ini?')) {
+                          handleDelete(product.id);
+                        }
+                      }}
+                    >
+                      Delete
+                    </Button>
+                    <Button variant="warning" href={`/edit-product/edit=${product.id}`}>
+                      Edit
+                    </Button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </Table>
+        <Button variant="primary" href="/add-product">
+          Tambahkan Produk
+        </Button>
+        <br />
+        <br />
+      </div>
+      <Footer />
+    </>
   );
 };
 
