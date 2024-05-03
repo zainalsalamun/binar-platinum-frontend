6e93ba527dbc4e62101181edc254834b02a12af3
binar-rizki <zainalrtf@gmail.com>
2024-05-03 16:56:58 +0700

    add env & app config

---
+++ src/config/app.config.js
@@ -0,0 +1,11 @@
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const REACT_APP_NODE_ENV = process.env.REACT_APP_NODE_ENV;
+
+const appConfig = {
+  apiUrl: REACT_APP_API_URL,
+  apiKey: API_KEY,
+  nodeEnv: REACT_APP_NODE_ENV
+};
+
+export default appConfig;
