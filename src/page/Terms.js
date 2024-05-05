5c4edbe48f48e777460947f6754324e92dd00f30
LukiLA1996 <lukila1996@gmail.com>
2024-05-05 12:58:53 +0700

    pakai style

--- src/page/Terms.js
+++ src/page/Terms.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React from 'react'
 import Footer from '../components/Dashboard/Footer'
 import NavBar from '../components/Dashboard/NavBar'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Terms = () => {
   return (
     <div>
       <NavBar />
-      <h2 className="col-12 py-5 text-center">Terms and Conditions</h2>
-      <h3 className="col-12 py-5 text-center">
-        Kami tahu bahwa Anda mungkin ingin melewati Persyaratan Layanan ini,
-        tetapi ini penting untuk menetapkan apa yang dapat Anda harapkan dari
-        kami saat Anda menggunakan layanan Ikea, dan apa yang kami harapkan
-        dari Anda.
-      </h3>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className="col-12 py-5 text-center">Terms and Conditions</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="col-12 py-5" style={{textAlign:'justify'}}>
+              Kami tahu bahwa Anda mungkin ingin melewati Persyaratan Layanan
+              ini, tetapi ini penting untuk menetapkan apa yang dapat Anda
+              harapkan dari kami saat Anda menggunakan layanan Ikea, dan apa
+              yang kami harapkan dari Anda.
+            </h3>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
-      <h2 />
     </div>
   )
 }
