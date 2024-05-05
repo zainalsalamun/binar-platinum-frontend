82dee78d325a99e4073dc27d478de780ff119b7e
LukiLA1996 <lukila1996@gmail.com>
2024-05-05 12:58:42 +0700

    pakai style

--- src/page/Privacy.js
+++ src/page/Privacy.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React from 'react'
 import Footer from '../components/Dashboard/Footer'
 import NavBar from '../components/Dashboard/NavBar'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Privacy = () => {
   return (
     <div>
       <NavBar />
-      <h2 className="col-12 py-5 text-center">Privacy Policy</h2>
-      <h3 className="col-12 py-5 text-center">
-        Dengan menggunakan layanan kami, Anda memercayakan informasi Anda kepada
-        kami. Kami paham bahwa melindungi informasi Anda dan memberikan kontrol
-        kepada Anda adalah tanggung jawab yang besar dan memerlukan kerja keras.
-      </h3>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className="col-12 py-5 text-center">Privacy Policy</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="col-12 py-5" style={{textAlign:'justify'}}>
+              Dengan menggunakan layanan kami, Anda memercayakan informasi Anda
+              kepada kami. Kami paham bahwa melindungi informasi Anda dan
+              memberikan kontrol kepada Anda adalah tanggung jawab yang besar
+              dan memerlukan kerja keras.
+            </h3>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
-      <h2 />
     </div>
   )
 }
