6e2e8061bed34dfe15b988c3de75f5c8999e8d76
LukiLA1996 <lukila1996@gmail.com>
2024-05-05 12:56:16 +0700

    perubahan hyperlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,22 @@
 const Navbar = () => {
-    return (
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/product-list">Product List</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
