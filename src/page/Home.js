b467b6324b81b03e418a3cc4a007b8c5e1b94862
LukiLA1996 <lukila1996@gmail.com>
2024-05-05 12:58:26 +0700

    penghapusan h2

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,30 +1,24 @@
-import React from "react";
-import Sliders from "../components/Dashboard/Sliders";
-import Footer from "../components/Dashboard/Footer";
-import Category from "../components/Dashboard/Category";
-import NavBar from "../components/Dashboard/NavBar";
-import Product from "../components/Product/Products";
+import React from 'react'
+import Sliders from '../components/Dashboard/Sliders'
+import Footer from '../components/Dashboard/Footer'
+import Category from '../components/Dashboard/Category'
+import NavBar from '../components/Dashboard/NavBar'
+import Product from '../components/Product/Products'
 
-
-
-const  Home = () =>  {
-
-    return (
-       <div>
+const Home = () => {
+  return (
+    <div>
       <NavBar />
-      <h2/>
+      <h2 />
       <Sliders />
-      <h1/>
+      <h1 />
       <Category />
-      <h1/>
-      <Product
-      />
-      <h2/>
+      <h1 />
+      <Product />
+      <h2 />
       <Footer />
-      <h2/>
-      
-  </div>
-    );
-  }
-export default Home;
-
+      <h2 />
+    </div>
+  )
+}
+export default Home
